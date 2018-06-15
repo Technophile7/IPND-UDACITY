0e37c718715ba8fdf147149ae60d34e4f1c556a4
Anish Malhotra <technophile.m7@gmail.com>
2018-06-15 23:13:20 +0530

    5th project added

---
+++ 5th pro/app/src/main/java/m7/ani/pizza_point/MainActivity.java
@@ -0,0 +1,13 @@
+package m7.ani.pizza_point;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
